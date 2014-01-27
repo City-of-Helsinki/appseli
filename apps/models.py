50487069751bc0c2fbf183a6574b22abac3e78d7
Rainer Koirikivi <rainer.koirikivi@arcusys.fi>
2014-01-27 13:44:33 +0200

    Minor model changes
    - PEP8 compatibility
    - Adding __unicode__ methods

--- apps/models.py
+++ apps/models.py
@@ -1,26 +1,40 @@
+from __future__ import unicode_literals
 from django.db import models
 
+
 class Platform(models.Model):
-    type = models.CharField(max_length=50) # web, android, ios, wp
-    name = models.CharField(max_length=100) # translatable
+    type = models.CharField(max_length=50)  # web, android, ios, wp
+    name = models.CharField(max_length=100)  # translatable
+
+    def __unicode__(self):
+        return self.name
+
 
 class Application(models.Model):
-    name = models.CharField(max_length=100) # translatable
+    name = models.CharField(max_length=100)  # translatable
     slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField() # translatable
+    description = models.TextField()  # translatable
     image = models.ImageField(upload_to='apps')
-    vendor = models.CharField(max_length=100) # should be ForeignKey?
-    rating = models.FloatField() # should be calculated automatically?
+    vendor = models.CharField(max_length=100)  # should be ForeignKey?
+    rating = models.FloatField()  # should be calculated automatically?
+
+    def __unicode__(self):
+        return self.name
+
 
 class ApplicationScreenshot(models.Model):
     application = models.ForeignKey(Application)
     platform = models.ForeignKey(Platform, null=True)
-    image = models.ImageField()
     index = models.PositiveSmallIntegerField()
     image = models.ImageField(upload_to='apps/screenshots')
 
     class Meta:
         unique_together = (('application', 'platform', 'index'),)
+        ordering = ('index',)
+
+    def __unicode__(self):
+        return "{} #{}".format(self.application, self.index)
+
 
 class ApplicationLanguageSupport(models.Model):
     language = models.CharField(max_length=5, db_index=True)
@@ -28,6 +42,10 @@ class ApplicationLanguageSupport(models.Model):
 
     class Meta:
         unique_together = (('language', 'application'),)
+
+    def __unicode__(self):
+        return "{}: {}".format(self.application, self.language)
+
 
 class ApplicationPlatformSupport(models.Model):
     platform = models.ForeignKey(Platform, db_index=True)
@@ -40,3 +58,5 @@ class ApplicationPlatformSupport(models.Model):
     class Meta:
         unique_together = (('platform', 'platform_link'), ('platform', 'application'))
 
+    def __unicode__(self):
+        return "{}: {}".format(self.application, self.platform)
