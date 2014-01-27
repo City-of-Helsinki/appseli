ab760b58bdfe3155fd6a8730318cf430bfb69ecf
Rainer Koirikivi <rainer.koirikivi@arcusys.fi>
2014-01-27 13:18:32 +0200

    Add django.contrib.messages to settings

--- appseli/settings.py
+++ appseli/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
     'modeltranslation',
     'rest_framework',
@@ -43,6 +44,7 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
