b09718f8c093b92b9417d06829897490e19eeebd
Rainer Koirikivi <rainer.koirikivi@arcusys.fi>
2014-01-31 19:14:50 +0200

    Show metadata for Applications' supported platforms

--- apps/serializers.py
+++ apps/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from .models import (
     Application,
+    ApplicationPlatformSupport,
     Platform,
 )
 
 
+class SupportedPlatformSerializer(serializers.ModelSerializer):
+    name = serializers.Field(source='platform.name')
+    type = serializers.Field(source='platform.type')
+    url = serializers.HyperlinkedRelatedField(view_name='platform-detail',
+                                              source='platform')
+
+    class Meta:
+        model = ApplicationPlatformSupport
+        fields = ('url', 'name', 'type', 'platform_link',
+                  'rating', 'nr_reviews', 'last_updated')
+
+
 class ApplicationSerializer(serializers.HyperlinkedModelSerializer):
-    # TODO: show the data from ApplicationPlatformSupport
+    platforms = SupportedPlatformSerializer(source='applicationplatformsupport_set',
+                                            many=True)
     languages = serializers.SlugRelatedField(many=True,
                                              read_only=True,
                                              slug_field='language')
